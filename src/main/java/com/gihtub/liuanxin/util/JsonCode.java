b6def779481995b592c947377fd57831cd6d6c80
liujie <liu_anxin@163.com>
2019-03-01 14:00:43 +0800

    fix

--- src/main/java/com/gihtub/liuanxin/util/JsonCode.java
+++ src/main/java/com/gihtub/liuanxin/util/JsonCode.java
@@ -5,15 +5,15 @@ public enum JsonCode {
 
     SUCCESS(200, "success. operate data or show message, depending on the specific business"),
 
-    BAD_REQUEST(400, "param error(show msg)"),
+    BAD_REQUEST(400, "param error(show response body)"),
 
     NOT_LOGIN(401, "need login, redirect to login page"),
 
-    NOT_PERMISSION(403, "need permission(show msg)"),
+    NOT_PERMISSION(403, "need permission(show response body)"),
 
     NOT_FOUND(404, "not found"),
 
-    FAIL(500, "internal error, service exception, request fail(show msg)")
+    FAIL(500, "internal error, service exception(show response body)")
 
     // , SERVICE_FAIL(1000, "service exception")
     ;
