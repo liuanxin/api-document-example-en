78e46b3d9b2b9296a7273bba8967c6f478a4c2c5
liujie <liu_anxin@163.com>
2019-03-01 12:55:55 +0800

    fix

--- src/main/java/com/gihtub/liuanxin/config/GlobalException.java
+++ src/main/java/com/gihtub/liuanxin/config/GlobalException.java
@@ -3,10 +3,13 @@ package com.gihtub.liuanxin.config;
 import com.gihtub.liuanxin.util.JsonCode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.Arrays;
 
 /**
  * @see org.springframework.boot.web.servlet.error.ErrorController
@@ -21,16 +24,26 @@ public class GlobalException {
 
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<String> noHandler(NoHandlerFoundException e) {
-        // debug log
+        // log
         String msg = String.format("Not found(%s %s)", e.getHttpMethod(), e.getRequestURL());
         return ResponseEntity.status(JsonCode.NOT_FOUND.getFlag()).body(msg);
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<String> missParam(MissingServletRequestParameterException e) {
-        // debug log
+        // log
         String msg = String.format("Missing required param(%s), type(%s)", e.getParameterName(), e.getParameterType());
         return ResponseEntity.status(JsonCode.BAD_REQUEST.getFlag()).body(msg);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<String> notSupported(HttpRequestMethodNotSupportedException e) {
+        // log
+        String msg = "not support method.";
+        if (!online) {
+            msg += String.format(". current(%s), support(%s)", e.getMethod(), Arrays.toString(e.getSupportedMethods()));
+        }
+        return ResponseEntity.status(JsonCode.FAIL.getFlag()).body(msg);
     }
 
 
@@ -47,7 +60,7 @@ public class GlobalException {
         } else {
             msg = e.getMessage();
         }
-        // error log
+        // log
         return ResponseEntity.status(JsonCode.FAIL.getFlag()).body(msg);
     }
 }
