5f725b3abb17b493bfd383efd88d99d9eb8aa357
liujie <liu_anxin@163.com>
2019-03-05 17:03:43 +0800

    fix

--- src/main/java/com/gihtub/liuanxin/web/ResponseExampleController.java
+++ src/main/java/com/gihtub/liuanxin/web/ResponseExampleController.java
@@ -7,18 +7,22 @@ import com.gihtub.liuanxin.exception.ServiceException;
 import com.gihtub.liuanxin.util.Page;
 import com.gihtub.liuanxin.vo.DemoVo;
 import com.github.liuanxin.api.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 @RestController
 @RequestMapping("/response")
 @ApiGroup("response")
 public class ResponseExampleController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResponseExampleController.class);
 
     @ApiMethod(title = "response mode", develop = Develop.PRODUCT, index = 3)
     @ApiResponses({
@@ -29,10 +33,40 @@ public class ResponseExampleController {
     public ResponseEntity<DemoVo> demoObject(@ApiParam(value = "product name", textarea = true) @RequestParam("name") String abc,
                                              @ApiParam(value = "head 1", paramType = ParamType.Header) @RequestHeader Long id,
                                              @ApiParam(value = "head 2", paramType = ParamType.Header) @RequestHeader("some") String xyz,
+                                             @ApiParam("upload file1") MultipartFile file1,
+                                             @ApiParam(value = "upload file2", must = true) MultipartFile file2,
                                              Page page) {
         if ("abc".equals(abc)) {
             throw new ServiceException("product name error");
         }
+        if (id == null || id < 0) {
+            throw new ServiceException("head1 can't null and can't less 0");
+        }
+        if (xyz == null || "".equals(xyz.trim())) {
+            throw new ServiceException("head2 can't null");
+        }
+        if (file2 == null) {
+            throw new ServiceException("upload file2 can't null");
+        }
+
+        String fileName = file2.getOriginalFilename();
+        if (fileName == null || !fileName.matches("(?i)^(.*)\\.(ico|jpeg|jpg|bmp|png)$")) {
+            throw new ServiceException("upload file2 must has image");
+        }
+
+        // save file
+        try {
+            String saveFile = UUID.randomUUID().toString().replace("-", "") + fileName.substring(fileName.lastIndexOf("."));
+            file2.transferTo(new File("/" + saveFile));
+        } catch (IOException e) {
+            String msg = "save file exception";
+            if (LOGGER.isErrorEnabled()) {
+                LOGGER.error(msg, e);
+            }
+            throw new ServiceException(msg);
+        }
+
+        // it's ok
         return ResponseEntity.ok(new DemoVo(123L, "Tom", Gender.Male, ProductType.Discount, null));
     }
 
@@ -55,14 +89,14 @@ public class ResponseExampleController {
 
     @ApiMethod(title = "response Map", develop = Develop.PRODUCT, index = 5)
     @ApiResponses({
-            @ApiResponse(code = 401, msg = "need login"),
+            @ApiResponse(code = 403, msg = "Returns when name was xyz, no permission(redirect to login page)"),
             @ApiResponse(code = 200, msg = "success")
     })
     @GetMapping("/demo-map")
     public ResponseEntity<Map<String, DemoVo>> demoMap(@ApiParam(value = "product name", textarea = true) String name,
                                                        Page page) {
         if ("xyz".equals(name)) {
-            return ResponseEntity.status(401).build();
+            return ResponseEntity.status(403).build();
         } else {
             return ResponseEntity.ok(Collections.singletonMap(
                     "123", new DemoVo(123L, "张三", Gender.Male, ProductType.Discount, null)
