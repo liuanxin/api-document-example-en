781e50035528df5a6d7818e1b905dcfc0d60a9e2
liujie <liu_anxin@163.com>
2019-08-13 15:23:25 +0800

    fix

---
+++ src/main/java/com/gihtub/liuanxin/dto/RequestBodyDto.java
@@ -0,0 +1,17 @@
+package com.gihtub.liuanxin.dto;
+
+import com.gihtub.liuanxin.enums.Gender;
+import com.github.liuanxin.api.annotation.ApiParam;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RequestBodyDto {
+
+    @ApiParam(value = "用户 id", must = true)
+    private Long userId;
+
+    @ApiParam(value = "性别", dataType = "int")
+    private Gender gender;
+}
--- src/main/java/com/gihtub/liuanxin/web/UserExampleController.java
+++ src/main/java/com/gihtub/liuanxin/web/UserExampleController.java
@@ -2,6 +2,7 @@ package com.gihtub.liuanxin.web;
 
 import com.gihtub.liuanxin.constant.Develop;
 import com.gihtub.liuanxin.dto.DemoDto;
+import com.gihtub.liuanxin.dto.RequestBodyDto;
 import com.gihtub.liuanxin.enums.UserType;
 import com.gihtub.liuanxin.exception.ServiceException;
 import com.gihtub.liuanxin.util.JsonResult;
@@ -10,6 +11,8 @@ import com.gihtub.liuanxin.util.PageInfo;
 import com.gihtub.liuanxin.vo.DemoVo;
 import com.github.liuanxin.api.annotation.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @ApiTokens(false)
 @RestController
@@ -47,6 +50,12 @@ public class UserExampleController {
             throw new ServiceException("operate error");
         }
         return JsonResult.success("test4");
+    }
+
+    @ApiMethod(value = "user change", develop = Develop.USER)
+    @PostMapping("/detail")
+    public JsonResult<List<DemoVo>> demo5(@RequestBody RequestBodyDto demoDto) {
+        return JsonResult.success("test5");
     }
 
     @ApiIgnore
