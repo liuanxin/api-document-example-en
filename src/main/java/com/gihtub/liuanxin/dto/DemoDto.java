ee173457478d6afbfb5e8e2c12f157b0b0eb19ad
liujie <liu_anxin@163.com>
2019-09-16 18:07:17 +0800

    fix

--- src/main/java/com/gihtub/liuanxin/dto/DemoDto.java
+++ src/main/java/com/gihtub/liuanxin/dto/DemoDto.java
@@ -7,6 +7,8 @@ import com.github.liuanxin.api.annotation.ApiParam;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class DemoDto {
@@ -20,6 +22,9 @@ public class DemoDto {
     // Document collection will add enum's { getCode : getValue } information (no splicing name) to the description
     @ApiParam(value = "user gender", dataType = "int")
     private Gender gender;
+
+    @ApiParam(datePattern = "MM/DD/YYYY HH:mm", must = true)
+    private Date date;
 
     public void basicCheck() {
         if (userId == null || userId <= 0) {
